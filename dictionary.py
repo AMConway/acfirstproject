b31efe31f5feb9e6e7830cd677a22e2a535da2cd
Amber Conway <amber.conway@metaswitch.com>
2017-04-12 11:35:44 +0100

    numbered definitions

--- dictionary.py
+++ dictionary.py
@@ -5,13 +5,14 @@ word_definitions = {'pen': [('noun ', 'A writing implement'), ('verb ', 'Write s
               'mobile phone': ['noun A portable telephone'],
               'small': ['adjective Less than average in size']}
 
-
+possible_definitions = (word_definitions[input()])
 # this function suggests a definition
 def suggest_definition(word):
 
 
     if word in word_definitions:
-        return word_definitions[word]
+
+        return possible_definitions
     else:
         possible_match = fuzzy_match(word)
         if possible_match:
@@ -33,16 +34,17 @@ def anything_else(response):
     if 'yes'in response:
         print('Type to see:\na) this word in a sentence\nb)this word translated into Italian\nc)' \
                        'a definition of a new word\nd)suggest an alternative definition to this word')
-        print(something_else(input()))
+        #print(something_else(input()))
     elif 'no' in response:
         print('bye')
         exit(0)
     else:
         print('Type to see:\na) this word in a sentence\nb)this word translated into Italian\nc)' \
                        'a definition of a new word\nd)suggest an alternative definition to this word')
-        print(something_else(input()))
+        #print(something_else(input()))'''
 
-def put_sentence(word):
+#notcomplete
+'''def put_sentence(word):
     sentence = dict()
     sentence = {'pen': ['Circle the answer using a red pen', 'He couldn\'t pen the story']}
     print (sentence[word])
@@ -60,17 +62,29 @@ def something_else(enter):
     elif 'translate' in enter:
               return translate(enter)
     else:
-              return 'other'
+              return 'other'''''
 
-
-
+lookup_word = input()
 anything ='Is there anything else we can help you with?'
-suggested_definition =suggest_definition(input())
+suggested_definition =suggest_definition(lookup_word)
+
 if suggested_definition != False:
-    print(suggested_definition)
-else:
-    print('Word not found, enter a new word')
+         print('There is {} definitions for {} in the dictionary.'.format(len(suggested_definition), lookup_word))
+
+         if len(suggested_definition) ==1:
+             print('{}.{}'.format((len(suggested_definition) - 1), possible_definitions[0])
+#problem here
+         else :
+             print('There are {} definitions for {} in the dictionary.'.format(len(suggested_definition), lookup_word))
+             print('{}.{}\n{}.{}'.format((len(suggested_definition) - 1), possible_definitions[0],
+                                         len(suggested_definition), possible_definitions[1]))
+
+
+      #for speech, definition in lookup_word:
+             # print('{}\n {}'.format(word_definitions[word[0]], word_definitions[word[1]])
+             # print('Word not found, enter a new word')
 
 
 print(anything)
+
 print(anything_else(input()))
--- hello.py
+++ hello.py
@@ -37,9 +37,40 @@ def print_spam():
 print(do_twice(print_spam))
 
 
+'''def nice_formatting(word_definitions[lookup_word]):
+    if suggested_definition != False:
+        print('There are {} definitions for {} in the dictionary.'.format(len(suggested_definition), lookup_word))
+        for speech, description in enumerate.(word_definitions[lookup_word]):
+            print('{} {}'.format(speech, description))
+            return'''
 
-def define(word):
-    definition = '1.noun A writing implement'
-    return(word + definition)
 
-print(define('pen'))
+
+
+
+'''if suggested_definition != False:
+    #print(suggested_definition)
+    print('There are {} definitions for {} in the dictionary.'.format(len(suggested_definition), lookup_word))
+    for definition in suggested_definition:
+        print('   Definition {} has {} parts to it.'.format(definition, len(definition)))
+else:
+    print('Word not found, enter a new word')
+
+for speech, description in enumerate.(suggested_definition):
+    print(speech,description)'''
+
+def anything_else(response):
+
+    if 'yes'in response:
+        print('Type to see:\na) this word in a sentence\nb)this word translated into Italian\nc)' \
+                       'a definition of a new word\nd)suggest an alternative definition to this word')
+        #print(something_else(input()))
+    elif 'no' in response:
+        print('bye')
+        exit(0)
+    else:
+        print('Type to see:\na) this word in a sentence\nb)this word translated into Italian\nc)' \
+                       'a definition of a new word\nd)suggest an alternative definition to this word')
+        #print(something_else(input()))
+
+print(anything_else(input()))
